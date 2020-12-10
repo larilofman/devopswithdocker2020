316f73f149335d8f1233875be346b5e1fc8f2b3f
Lari Löfman <larwazor@gmail.com>
2020-12-10 16:53:30 +0200

    modify exercise 3.3

--- part3/exercise3.3/scripti.sh
+++ part3/exercise3.3/scripti.sh
@@ -4,11 +4,14 @@
 # 2: github repo (same will be used for docker hub)
 # 3: docker username
 
+# Clone repository
 repo="https://github.com/"$1"/"$2".git"
 git clone $repo
 
+# Build Dockerfile
 cd $2
-docker build -t $2 .
-docker tag script_test $3"/"$2
+docker build -t $3"/"$2 .
+
+# Log into docker and push the image
 docker login
 docker push $3"/"$2
